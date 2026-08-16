efd84310753aa539768560161b1290a2b4fea1dd
mnc1337-studying <coolpon845@gmail.com>
2026-08-16 23:09:15 +0300

    Homework №18 added

---
+++ homework_18/index.js
@@ -0,0 +1,18 @@
+// Завдання №1
+let myNum = 10;
+let myStr = 'some string';
+let myBool = true;
+let myArr = [1, 2, 3, 4, 5];
+let myObj = {
+    first: 'First Name',
+    last: 'Last Name'
+};
+
+
+// Завдання №2
+let decimal2 = Number(myNum.toFixed(2));
+
+
+// Завдання №3
+let myBigInt = 123n;
+myBigInt++;
